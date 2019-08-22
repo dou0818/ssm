36bf80f580965399b281372d91cdd634d4b6f574
luo2646212315 <2646212315@qq.com>
2019-08-22 11:12:06 +0800

    更新

---
+++ src/main/java/com/stock/commons/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.stock.commons;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * 全局异常处理类
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
