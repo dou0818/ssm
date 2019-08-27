a9812c58712bfffcf257085217c1d12958364905
xiaoqi666666 <2037336761@qq.com>
2019-08-27 15:39:39 +0800

    vvcvc

--- src/main/java/com/stock/util/RediusUtil.java
+++ src/main/java/com/stock/util/RediusUtil.java
@@ -4,5 +4,17 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class RediusUtil {
+	 @Resource(name = "redisTemplate2")
+    RedisTemplate redisTemplate;
+
+    public boolean set(String key, String value, long time) {
+        redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+        return true;
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
 
 }
