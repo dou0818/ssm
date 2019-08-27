a0523c0e3a62e804f10fa2017f78ee6b7d812d5d
xiaoqi666666 <2037336761@qq.com>
2019-08-27 15:45:03 +0800

    a

--- src/main/java/com/stock/commons/GlobalExceptionHandler.java
+++ src/main/java/com/stock/commons/GlobalExceptionHandler.java
@@ -1,11 +1,26 @@
 package com.stock.commons;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/***
  * 全局异常处理类
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public RestFulResponseBody BindExceptionHandler(HttpServletRequest request,
+                                       final Exception e, HttpServletResponse response) {
+        System.out.println("BindException错误");
+        RestFulResponseBody rs=  new RestFulResponseBody("参数错误", HttpStatus.INTERNAL_SERVER_ERROR);
+        return rs;
+    }
 }
