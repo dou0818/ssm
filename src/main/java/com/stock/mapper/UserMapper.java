b9c5185d56948034d9f92ffebbed299b51dddb07
dou0818 <601383494@qq.com>
2019-08-27 16:35:43 +0800

    aaa

--- src/main/java/com/stock/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.stock.mapper;
-
-import com.stock.domain.User;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserMapper extends Mapper<User> {
-
-}
--- src/main/java/com/stock/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.stock.service;
-
-public interface UserService{
-
-
-}
