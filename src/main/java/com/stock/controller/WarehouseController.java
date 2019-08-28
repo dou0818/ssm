c445153035d6fe02a37887153b656082a6e02bbd
luo2646212315 <2646212315@qq.com>
2019-08-28 11:16:07 +0800

    s

--- src/main/java/com/stock/controller/WarehouseController.java
+++ src/main/java/com/stock/controller/WarehouseController.java
@@ -1,22 +1,32 @@
 package com.stock.controller;
 
 import com.stock.commons.MyResponseRestful;
+import com.stock.service.WarehouseService;
+import com.stock.util.ArrayToListUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 public class WarehouseController {
-    public MyResponseRestful salesReturn(List<Map<String,Object>> salesList){
-        MyResponseRestful restful = new MyResponseRestful();
+    @Resource(name = "warehouseServiceImpl")
+    WarehouseService warehouseService;
 
+    @RequestMapping(value = "/salesReturn")
+    public MyResponseRestful salesReturn(int[] commodityids, int[] merchantids, int[] nums, int[] warehouseids) {
+        List<Map<String, Object>> wareList = ArrayToListUtils.wareList(commodityids, merchantids, nums, warehouseids);
+        boolean flag = warehouseService.salesReturn(wareList);
+        MyResponseRestful restful = new MyResponseRestful(HttpStatus.OK,"退货入库成功");
         return restful;
     }
+
     @RequestMapping("/s")
-    public String a(int[] aid, int[] bid){
-        System.out.println("aid +\"\\t\"+bid = " + aid +"\t"+bid);
+    public String a(int[] aid, int[] bid) {
+        System.out.println("aid +\"\\t\"+bid = " + aid + "\t" + bid);
         for (int i : aid) {
             System.out.println("i = " + i);
         }
--- src/main/java/com/stock/util/ArrayToListUtils.java
+++ src/main/java/com/stock/util/ArrayToListUtils.java
@@ -9,55 +9,55 @@ import java.util.Map;
  * 将数组转化为需要的list<map>
  */
 public class ArrayToListUtils {
-/**
- * 销售层数组转化成list
- *
- * @param commodityids
- * @param merchantids
- * @param nums
- * @return
- */
-public static List<Map<String, Object>> saleList(int[] commodityids, int[] merchantids, int[] nums) {
-    List<Map<String, Object>> saleList = new ArrayList<>();
-    for (int i=0;i<commodityids.length;i++) {
-        Map<String,Object> saleMap = new HashMap<>();
-        saleMap.put("commodityid",commodityids[i]);
-        saleMap.put("merchantid",merchantids[i]);
-        saleMap.put("num",nums[i]);
-        saleList.add(saleMap);
+    /**
+     * 销售层数组转化成list
+     *
+     * @param commodityids
+     * @param merchantids
+     * @param nums
+     * @return
+     */
+    public static List<Map<String, Object>> saleList(int[] commodityids, int[] merchantids, int[] nums) {
+        List<Map<String, Object>> saleList = new ArrayList<>();
+        for (int i = 0; i < commodityids.length; i++) {
+            Map<String, Object> saleMap = new HashMap<>();
+            saleMap.put("commodityid", commodityids[i]);
+            saleMap.put("merchantid", merchantids[i]);
+            saleMap.put("num", nums[i]);
+            saleList.add(saleMap);
+        }
+        return saleList;
     }
-    return saleList;
-}
 
-/**
- * 仓库层
- *
- * @param commodityids
- * @param merchantids
- * @param nums
- * @param warehouseids
- * @return
- */
-public static List<Map<String, Object>> wareList(int[] commodityids, int[] merchantids, int[] nums, int[] warehouseids) {
-    List<Map<String, Object>> wareList = new ArrayList<>();
-    for (int i=0;i<commodityids.length;i++) {
-        Map<String,Object> wareMap = new HashMap<>();
-        wareMap.put("commodityid",commodityids[i]);
-        wareMap.put("merchantid",merchantids[i]);
-        wareMap.put("num",nums[i]);
-        wareMap.put("warehouseid",warehouseids[i]);
-        wareList.add(wareMap);
+    /**
+     * 仓库层
+     *
+     * @param commodityids
+     * @param merchantids
+     * @param nums
+     * @param warehouseids
+     * @return
+     */
+    public static List<Map<String, Object>> wareList(int[] commodityids, int[] merchantids, int[] nums, int[] warehouseids) {
+        List<Map<String, Object>> wareList = new ArrayList<>();
+        for (int i = 0; i < commodityids.length; i++) {
+            Map<String, Object> wareMap = new HashMap<>();
+            wareMap.put("commodityid", commodityids[i]);
+            wareMap.put("merchantid", merchantids[i]);
+            wareMap.put("num", nums[i]);
+            wareMap.put("warehouseid", warehouseids[i]);
+            wareList.add(wareMap);
+        }
+        return wareList;
     }
-    return wareList;
-}
 
     public static void main(String[] args) {
-        int[] a= {1,2,3};
-        int[] b= {11,22,33};
-        int[] c= {111,222,333};
-     List<Map<String,Object>> s =   ArrayToListUtils.saleList(a,b,c);
+        int[] a = {1, 2, 3};
+        int[] b = {11, 22, 33};
+        int[] c = {111, 222, 333};
+        List<Map<String, Object>> s = ArrayToListUtils.saleList(a, b, c);
         for (Map<String, Object> map : s) {
-            int aa= (int) map.get("commodityid");
+            int aa = (int) map.get("commodityid");
             System.out.println("aa = " + aa);
         }
     }
