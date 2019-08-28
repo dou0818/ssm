d8b3d6e18a792c05c5327a36014a37f491140051
luo2646212315 <2646212315@qq.com>
2019-08-28 10:37:04 +0800

    a

--- src/main/java/com/stock/service/WarehouseService.java
+++ src/main/java/com/stock/service/WarehouseService.java
@@ -1,6 +1,14 @@
 package com.stock.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface WarehouseService{
 
-
+    /**
+     * 用户退货后进行的操作
+     * @param salesList
+     * @return boolean
+     */
+    boolean salesReturn(List<Map<String,Object>> salesList);
 }
--- src/main/java/com/stock/service/WarehouseServiceImpl.java
+++ src/main/java/com/stock/service/WarehouseServiceImpl.java
@@ -1,13 +1,31 @@
 package com.stock.service;
 
-import org.springframework.stereotype.Service;
-import javax.annotation.Resource;
 import com.stock.mapper.WarehouseMapper;
-import com.stock.service.WarehouseService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+
 @Service
 public class WarehouseServiceImpl implements WarehouseService{
 
     @Resource
     private WarehouseMapper warehouseMapper;
 
+
+    /**
+     * 用户退货后进行的操作
+     *
+     * @param salesList
+     * @return boolean
+     */
+    @Override
+    public boolean salesReturn(List<Map<String, Object>> salesList) {
+        for (Map<String, Object> salesMap : salesList) {
+
+        }
+        return false;
+    }
 }
