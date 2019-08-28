3d78e3529e8f1a8a387df7356a077d2b36e7a1bc
qiao1101201300 <1252925124@qq.com>
2019-08-28 10:52:41 +0800

    添加活动

--- src/main/java/com/stock/service/SaletableService.java
+++ src/main/java/com/stock/service/SaletableService.java
@@ -1,6 +1,12 @@
 package com.stock.service;
 
+import com.stock.domain.Saletable;
+
+import java.util.List;
+
 public interface SaletableService{
 
+    int addActivityNum(int merchanid, int commodityid, int num);
 
+    List<Saletable> selAllSaleNum();
 }
