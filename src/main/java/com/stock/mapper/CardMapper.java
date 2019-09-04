59073a433c3007877f575d08ed4122d2f111b9c7
dou0818 <601383494@qq.com>
2019-09-04 14:18:29 +0800

    aa

--- src/main/java/com/stock/mapper/CardMapper.java
+++
@@ -1,7 +0,0 @@
-package com.stock.mapper;
-
-import com.stock.domain.Card;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface CardMapper extends Mapper<Card> {
-}
--- src/main/java/com/stock/service/CardService.java
+++
@@ -1,8 +0,0 @@
-package com.stock.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface CardService{
-
-}
