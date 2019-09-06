bdc04b586d399009efecd4e93a73392e252700cc
dou0818 <601383494@qq.com>
2019-09-06 14:56:10 +0800

    aaa

--- src/main/java/com/stock/mapper/SaletableMapper.java
+++
@@ -1,7 +0,0 @@
-package com.stock.mapper;
-
-import com.stock.domain.Saletable;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface SaletableMapper extends Mapper<Saletable> {
-}
--- src/main/java/com/stock/mapper/WarehouseMapper.java
+++
@@ -1,7 +0,0 @@
-package com.stock.mapper;
-
-import com.stock.domain.Warehouse;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface WarehouseMapper extends Mapper<Warehouse> {
-}
